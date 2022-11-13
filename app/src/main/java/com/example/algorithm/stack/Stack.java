d61180bccb01983d153363b976cdb81653473b8a
ARCHANA-KUMARI <ms.archanacusat2015@gmail.com>
2022-11-13 14:19:06 +0530

    Fixed size Stack refactoring

--- app/src/main/java/com/example/algorithm/stack/Stack.java
+++ app/src/main/java/com/example/algorithm/stack/Stack.java
@@ -6,12 +6,12 @@ package com.example.algorithm.stack;
  */
 public class Stack {
     private final int SIZE;
-    private int container[];
-    private int top = -1;
+    private int mContainer[];
+    private int mTop = -1;
 
     public Stack(int size) {
         SIZE = size;
-        container = new int[SIZE];
+        mContainer = new int[SIZE];
     }
 
     public void push(int data) {
@@ -19,8 +19,8 @@ public class Stack {
             System.out.println("Stack Overflow");
             return;
         }
-        top = top + 1;
-        container[top] = data;
+        mTop = mTop + 1;
+        mContainer[mTop] = data;
     }
 
     public int pop() {
@@ -28,19 +28,19 @@ public class Stack {
             System.out.println("Stack Underflow");
             return -1;
         }
-        int data = container[top];
-        top = top - 1;
+        int data = mContainer[mTop];
+        mTop = mTop - 1;
         return data;
     }
 
     private boolean isEmpty() {
-        if (top == -1)
+        if (mTop == -1)
             return true;
         else return false;
     }
 
     private boolean isFull() {
-        if (top == SIZE - 1)
+        if (mTop == SIZE - 1)
             return true;
         else return false;
     }
@@ -50,7 +50,7 @@ public class Stack {
             System.out.println("Stack underflow");
             return -1;
         }
-        return container[top];
+        return mContainer[mTop];
     }
 
     public void display() {
@@ -58,8 +58,8 @@ public class Stack {
             System.out.println("Stack underFlow");
             return;
         }
-        for (int i = 0; i <= top; i++) {
-            System.out.print(container[i] + "\t");
+        for (int i = 0; i <= mTop; i++) {
+            System.out.print(mContainer[i] + "\t");
         }
         System.out.println();
     }
---
+++ app/src/main/java/com/example/algorithm/stack/stackApplication/StackReverseString.java
@@ -0,0 +1,17 @@
+package com.example.algorithm.stack.stackApplication;
+
+import java.util.Scanner;
+
+/**
+ * @Author: Archana Kumari
+ * @Date: 13-11-2022
+ */
+public class StackReverseString {
+    public static void main(String[] args) {
+        System.out.println("Enter string");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+
+    }
+}
