e19cfba317f416e2cb8634a401873a8d04a7dd6b
Archana Kumari <ms.archanacusat2015@gmail.com>
2023-01-08 14:06:38 +0530

    Added second way to do count sort

--- app/src/main/java/com/example/algorithm/sorting/LinkedListSorting/CountSort.java
+++ app/src/main/java/com/example/algorithm/sorting/LinkedListSorting/CountSort.java
@@ -32,11 +32,38 @@ public class CountSort {
         return arr;
     }
 
+    public static int[] countSort2ndWay(int arr[]) {
+        int max = Utils.getMax(arr);
+        System.out.println("Max is:" + max);
+        // Count array
+        int countArray[] = new int[max + 1];
+        for (int i = 0; i < countArray.length; i++) {
+            countArray[i] = 0;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            countArray[arr[i]] = countArray[arr[i]] + 1;
+        }
+
+        for (int i = 1; i < countArray.length; i++) {
+            countArray[i] = countArray[i - 1] + countArray[i] ;
+        }
+
+       int outputArray[] = new int[arr.length];
+
+       for(int i = arr.length - 1 ; i >= 0; i--){
+           countArray[arr[i]] = countArray[arr[i]] - 1;
+           outputArray[countArray[arr[i]]] = arr[i];
+       }
+        return outputArray;
+    }
+
+
     public static void main(String[] args) {
         //int arr[] = { 0, 1, 4, 1, 2, 7, 5, 2};
         int arr[] = { 0, 1, 4, 1, 2, 77000, 5, 2};
-        //int arr[] = { 0, 1, 4, 1, 2, -77000, 5, 2};
-        arr = countSort(arr);
+        // int arr[] = { 0, 1, 4, 1, 2, -77000, 5, 2};
+        //arr = countSort(arr);
+        arr = countSort2ndWay(arr);
 
         // Print sorted list
         for(int i = 0 ; i < arr.length ; i++){
