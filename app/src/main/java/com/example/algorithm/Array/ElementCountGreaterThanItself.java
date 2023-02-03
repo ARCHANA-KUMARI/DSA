2af42d7e9ba849c83668b8ce007cb3f90adb60c2
Archana Kumari <ms.archanacusat2015@gmail.com>
2023-02-03 18:04:08 +0530

    Added second apporach for finding ElementCounterGreaterThanItself

--- app/src/main/java/com/example/algorithm/Array/ElementCountGreaterThanItself.java
+++ app/src/main/java/com/example/algorithm/Array/ElementCountGreaterThanItself.java
@@ -11,16 +11,33 @@ import com.example.algorithm.Utils;
 public class ElementCountGreaterThanItself {
 
     public static void main(String args[]) {
-        //int array[] = new int[]{-3, -2, 6, 8, 4, 8, 5}; // ans = 5
+        int array[] = new int[]{-3, -2, 6, 8, 4, 8, 5}; // ans = 5
         //int array[] = new int[]{2, 5, 1, 4, 8, 0, 8, 1, 3, 8}; // ans = 7
-        int array[] = new int[]{1,1,1,1,1};
-        System.out.println("Element count which is greater than itself" + countElementGreaterThanItself(array));
+        //int array[] = new int[]{1,1,1,1,1};
+       // System.out.println("Element count which is greater than itself" + countElementGreaterThanItself(array));
+        System.out.println("Element count which is greater than itself" + countElementGreaterThanItselfInOneLoop(array));
     }
 
     public static long countElementGreaterThanItself(int array[]) {
         int max = Utils.getMax(array);
         int maxFreq = 0;
         for (int i = 0; i < array.length; i++) {
+            if (array[i] == max)
+                maxFreq = maxFreq + 1;
+        }
+        return array.length - maxFreq;
+    }
+
+    public static long countElementGreaterThanItselfInOneLoop(int array[]) {
+        int maxFreq = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            // Find max here only and count freq of maximum element of array
+            if(max < array[i])
+            {
+                max = array[i];
+                maxFreq = 0;
+            }
             if (array[i] == max)
                 maxFreq = maxFreq + 1;
         }
--- app/src/main/java/com/example/algorithm/Array/bruteforce/TripletCounter.java
+++ app/src/main/java/com/example/algorithm/Array/bruteforce/TripletCounter.java
@@ -13,6 +13,7 @@ public class TripletCounter {
         int temp = 0;
         int tripletCount = 0;
         for (int i = 0; i < A.size() - 1; i++) {
+            temp =0;
             for (int j = i + 1; j < A.size(); j++) {
                 if (temp > A.get(i) && temp < A.get(j)) {
                     tripletCount++;
@@ -25,25 +26,36 @@ public class TripletCounter {
     }
 
     public static void main(String[] args) {
-   /*    // A = [1, 2, 4, 3]
-        ArrayList list = new ArrayList();
+       // A = [1, 2, 4, 3]
+      /*  ArrayList list = new ArrayList();
         list.add(1);
         list.add(2);
         list.add(4);
         list.add(3);*/
-        /* A = [2, 1, 2, 3]*/
-
-       /* ArrayList list = new ArrayList();
-        list.add(2);
-        list.add(1);
-        list.add(2);
-        list.add(3);*/
+       //  A = [2, 1, 2, 3]
 
         ArrayList list = new ArrayList();
+        list.add(2);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+   /*     ArrayList list = new ArrayList();
         list.add(1);
         list.add(1);
         list.add(1);
-        list.add(1);
+        list.add(1);*/
+    /*  //  A : [ 70888, 2417, 136, 95979, 93360, 55711, 80569 ]
+        ArrayList list = new ArrayList();
+        list.add(70888);
+        list.add(2417);
+        list.add(136);
+        list.add(95979);
+        list.add(93360);
+        list.add(55711);
+        list.add(80569);*/
+
+      //  A : [ 19693, 90763, 23586, 18259, 15715, 8892, 1366, 8309, 66170, 19072, 14992, 91483, 97245, 48005, 33421, 71066, 13466, 30368, 64109, 16774, 75716, 46089, 16630, 48154, 54068, 90749, 79857, 23699, 36068, 16539, 75467, 70069, 8531, 74888, 45046, 63364, 1700, 46827, 66181, 84044, 5470, 16145, 15859 ]
         System.out.println("No of Triplet count:" + getTripletCount(list));
     }
 }
