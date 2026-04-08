e4059f67e20e455a68a0008f89b2f3d1330683ac
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-04-08 19:56:15 +0530

    Refactored the code

--- app/src/main/java/com/example/algorithm/Algo/Math/bruitforce/FindAllFactorsCount.java
+++ app/src/main/java/com/example/algorithm/Algo/Math/bruitforce/FindAllFactorsCount.java
@@ -6,15 +6,15 @@ package com.example.algorithm.Algo.Math.bruitforce;
 
 public class FindAllFactorsCount {
     public static void main(String[] args) {
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(24));
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(100));
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(25));
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(1));
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(0));
-        System.out.println("NO of factors of Given number:" + calculateNumberOfFactorsOfTheNumber(-1));
+        System.out.println("NO of factors of Given number:" + countFactors(24));
+        System.out.println("NO of factors of Given number:" + countFactors(100));
+        System.out.println("NO of factors of Given number:" + countFactors(25));
+        System.out.println("NO of factors of Given number:" + countFactors(1));
+        System.out.println("NO of factors of Given number:" + countFactors(0));
+        System.out.println("NO of factors of Given number:" + countFactors(-1));
     }
 
-    private static int calculateNumberOfFactorsOfTheNumber(int num) {
+    private static int countFactors(int num) {
         if (num <= 0)
             return -1;
         int count = 0;
