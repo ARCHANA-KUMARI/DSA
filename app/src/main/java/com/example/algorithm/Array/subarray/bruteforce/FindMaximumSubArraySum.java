23c3e629610a8a620195a547aaf14f2c41c71b84
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-05-30 15:18:55 +0530

    MaxSubArraySum inital commit.

---
+++ app/src/main/java/com/example/algorithm/Array/subarray/bruteforce/FindMaximumSubArraySum.java
@@ -0,0 +1,28 @@
+/**
+ * Author: Archana Kumari
+ * Date: 5/30/26
+ * File: FindMaximumSubArraySum.java
+ */
+
+package com.example.algorithm.Array.subarray.bruteforce;
+
+public class FindMaximumSubArraySum {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println("findMaxSubArraySum(arr) = " + findMaxSubArraySum(arr));
+    }
+    
+    private static int findMaxSubArraySum(int array[]){
+        int maxSubArraySum = array[0];
+
+        for(int i = 0; i < array.length;i++){
+            int currentSubArraySum = 0;
+            for(int j = i ; j<array.length; j++){
+                currentSubArraySum = currentSubArraySum + array[j];
+                maxSubArraySum = Math.max(maxSubArraySum, currentSubArraySum);
+            }
+        }
+        return maxSubArraySum;
+    }
+
+}
